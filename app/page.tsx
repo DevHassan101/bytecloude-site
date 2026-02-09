354615cc4d64c59880fd8a0e328b97aaa7361bed
Hassan Shaikh <147030057+DevHassan101@users.noreply.github.com>
2026-02-09 14:39:52 +0500

    dynamic-load

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,24 @@
 
 import AboutUs from "./components/AboutUs";
 import HeroBanner from "./components/HeroBanner";
-import OurPortfolio from "./components/OurPortfolio";
+// import OurPortfolio from "./components/OurPortfolio";
 import OurServices from "./components/OurServices";
-import OurTestimonials from "./components/OurTestimoinals";
+// import OurTestimonials from "./components/OurTestimoinals";
 import StatsSection from "./components/StatsSection";
+import dynamic from 'next/dynamic';
+
 
 export default function Home() {
+  // ⭐ Heavy components ko lazy load karo
+  const OurPortfolio = dynamic(() => import('./components/OurPortfolio'), {
+    loading: () => <div>Loading...</div>,
+    ssr: true, // Server-side rendering enabled
+  });
+
+  const OurTestimonials = dynamic(() => import('./components/OurPortfolio'), {
+    loading: () => <div>Loading...</div>,
+    ssr: false, // Client-side only
+  });
   return (
     <>
       <HeroBanner />
