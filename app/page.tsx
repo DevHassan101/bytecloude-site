9e15ba89c62a3235f648e1e20833d3a47901a2fc
Hassan Shaikh <147030057+DevHassan101@users.noreply.github.com>
2026-02-09 15:54:38 +0500

    ok,good

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-'use client'
-
 import AboutUs from "./components/AboutUs";
 import HeroBanner from "./components/HeroBanner";
 import OurServices from "./components/OurServices";
-import OurTestimonials from "./components/OurTestimoinals";
 import StatsSection from "./components/StatsSection";
 import dynamic from 'next/dynamic';
 
 const OurPortfolio = dynamic(() => import('./components/OurPortfolio'), {
+  loading: () => <div>Loading...</div>,
+  ssr: true,
+});
+
+const OurTestimonials = dynamic(() => import('./components/OurTestimoinals'), {
   loading: () => <div>Loading...</div>,
   ssr: true,
 });
